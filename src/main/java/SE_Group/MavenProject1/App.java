1c3fbcfde3105df3f5acd082a2a989d1c33e989f
pranava3204 <143255082+pranava3204@users.noreply.github.com>
2024-01-04 14:53:38 +0530

    Update App.java

--- src/main/java/SE_Group/MavenProject1/App.java
+++ src/main/java/SE_Group/MavenProject1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!Goodmmmm Morning" );
+        System.out.println( "Hello World!GoodMorning" );
     }
 }
